794f723f28f3a91ae48297314a88d1eaeca535cb
Andres Allievi <garzamorada@gmail.com>
2021-12-29 20:20:06 -0300

    casos conjurados que no degradan

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -127,6 +127,27 @@ export class GildedRose {
                     } else {
                         this.items[i].quality = this.items[i].quality - 4
                     }    
+                } else {
+                    if (this.items[i].name.indexOf('Aged Brie') !== -1 ) {
+                        if (this.items[i].quality < 50) {
+                            this.items[i].quality = this.items[i].quality + 1
+                        }
+                    } else if (this.items[i].name.indexOf('Backstage passes to a TAFKAL80ETC concert') !== -1 ) {
+                        if (this.items[i].sellIn < 0 ) {
+                            this.items[i].quality = 0
+                        }
+                        if (this.items[i].sellIn < 6 ) {
+                            this.items[i].quality = this.items[i].quality + 3
+                        } else if (this.items[i].sellIn < 11 ) {
+                            this.items[i].quality = this.items[i].quality + 2                                
+                        } else {
+                            this.items[i].quality = this.items[i].quality + 1                                
+                        }
+
+                    } else  if (this.items[i].name.indexOf('Sulfuras, Hand of Ragnaros') !== -1) {
+                        this.items[i].quality = 80
+                    }
+                    
                 }
             }
             if this.items[i].name.indexOf('Sulfuras, Hand of Ragnaros') == -1){ /* revisa el maximo y el minino de calidad, excluye Sulfuras */
